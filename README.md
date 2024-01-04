b246ab56c727c30427bb8696b9d23ece89df5b16
VibhuRajput <39965391+VibhuRajput@users.noreply.github.com>
2024-01-04 22:32:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stock-graph
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/VibhuRajput/stock-graph)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Stock from './Stock';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Stock></Stock>
+    </div>
+  );
+}
